dfdeb8c97b2a8038e8472772e4db922f53e6e0e5
neilsen zulueta <neilzulueta@gmail.com>
2024-02-22 02:35:13 -0800

    added image in aboutme file

--- react-portfolio/src/pages/Aboutme.jsx
+++ react-portfolio/src/pages/Aboutme.jsx
@@ -1,20 +1,31 @@
+import React from 'react';
 import Card from 'react-bootstrap/Card';
-import '../styles/aboutme.css'
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import '../styles/aboutme.css'; 
 
 export default function Aboutme() {
   return (
-    <Card>
-      <Card.Header>About Me</Card.Header>
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p>
-            {' '}
-            I'm a passionate and driven coding bootcamp student with a strong commitment to succeed. I believe the main standard to succeed in learning web development is hard work and willing to give my all in order to achieve positive results.{' '}
-          </p>
-         
-        </blockquote>
-      </Card.Body>
-    </Card>
+    <Container>
+      <Row className="align-items-center"> 
+        <Col xs={8} md={3} className="mb-3 mb-md-0"> 
+          <Image src="./public/assets/neilsenimage.jpeg" roundedCircle className="aboutme-image" />
+        </Col>
+        <Col xs={12} md={8}>
+          <Card>
+            <Card.Header>About Me</Card.Header>
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <p>
+                  I'm a passionate and driven coding bootcamp student with a strong commitment to succeed. I believe the main standard to succeed in learning web development is hard work and willing to give my all in order to achieve positive results.
+                </p>
+              </blockquote>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
